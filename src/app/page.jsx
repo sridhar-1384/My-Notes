33018f594b32addde1192e89f270a4c7169d8ec8
sridhar arasada <142651035+sridhar-1384@users.noreply.github.com>
2024-02-28 22:07:52 +0530

    update UI-2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default async function Home() {
         <div className="h-fit p-5 my-auto flex flex-col gap-y-4 justify-center items-center">
           <p className="text-5xl font-mono">Welcome {data && data.user.name} {!data && "to My Notes.."}</p>
           <p className="text-2xl font-mono">Keep your notes here...</p>
-          <Link href="/notes" className="w-fit hover:bg-[#2d355e] px-2 py-1 border-blue-200 rounded-md text-blue-500 font-semibold bg-[#535C91]">Notes</Link>
+          <Link href="/notes" className="border-white border-[1px] hover:bg-purple-700 focus:bg-purple-500 focus:border-none rounded-md px-2 py-0.5 bg-purple-950">Notes</Link>
         </div>
     </div>
   );
