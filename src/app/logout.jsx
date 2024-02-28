579f66c754d00830c3ce8a49d4b014ccf96af931
sridhar arasada <142651035+sridhar-1384@users.noreply.github.com>
2024-02-28 22:07:28 +0530

    update UI-1

--- src/app/logout.jsx
+++ src/app/logout.jsx
@@ -3,6 +3,6 @@ import { signOut } from "next-auth/react"
 import Link from "next/link"
 export default function Logout(){
     return (
-        <Link href="/" onClick={()=>(signOut())} className="border-white border-[1px] hover:bg-gray-700 focus:bg-gray-800 focus:border-none rounded-md px-2 py-0.5">Logout</Link>
+        <Link href="/" onClick={()=>(signOut())} className="border-white border-[1px] hover:bg-purple-700 focus:bg-purple-500 focus:border-none rounded-md px-2 py-0.5 bg-purple-950">Logout</Link>
     )
 }
